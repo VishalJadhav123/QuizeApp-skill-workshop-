3112fb4656be367202f1ce45957a6882b9ec36e3
VishalJadhav123 <vishaljadhav.2315@gmail.com>
2024-07-29 14:52:27 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,459 @@
+let questionSet = [
+  (history = [
+    {
+      que: "Who among the following ruler was defeated by Seleucus?",
+      a: "Chandra Gupta Maurya",
+      b: "Vikramaditya",
+      c: "Chanakya",
+      d: "None of the above",
+      ans: "a",
+    },
+    {
+      que: "What is Capital of Maharashtra",
+      a: "Mumbai",
+      b: "Pune",
+      c: "Kolhapur",
+      d: "Nashik",
+      ans: "a",
+    },
+    {
+      que: "Where did Tebhega movement occur in 1946?",
+      a: " Tamil Nadu",
+      b: " Karnataka",
+      c: "Bengal",
+      d: "Maharashtra",
+      ans: "c",
+    },
+    {
+      que: " Who wrote Bande Mataram?",
+      a: " Bankimchandra Chatterjee",
+      b: "Rabindranath Tagore",
+      c: "Sharat chandra chattopadhyay",
+      d: "None of the above",
+      ans: "b",
+    },
+    {
+      que: "When Indian national Anthem was first sung? ",
+      a: "August 15, 1947",
+      b: "1857 revolt",
+      c: " December 27, 1911 Calcutta",
+      d: "None of the Above",
+      ans: "c",
+    },
+    {
+      que: "What is the capital of Tamil Nadu?",
+      a: "delhi",
+      b: "mumbai",
+      c: "chennai",
+      d: "kolkata",
+      ans: "c",
+    },
+    {
+      que: "What is the capital of West Bengal?",
+      a: "delhi",
+      b: "mumbai",
+      c: "chennai",
+      d: "kolkata",
+      ans: "d",
+    },
+    {
+      que: "Where is situated the battlefield of Plessey?",
+      a: "Punjab",
+      b: "Haryana",
+      c: "West Bengal",
+      d: " Karnataka",
+      ans: "c",
+    },
+    {
+      que: "What was the name of Harsha’s Capital?",
+      a: "Kannaauj",
+      b: " Kanpur",
+      c: "Lucknow",
+      d: "Gaya",
+      ans: "a",
+    },
+    {
+      que: "Where did the formation of Azad Hind Fauj take Place?",
+      a: "Thailand",
+      b: "Haryana",
+      c: "Singapore",
+      d: " Karnataka",
+      ans: "c",
+    },
+  ]),
+  (javascript = [
+    {
+      que: " Which type of JavaScript language is ___",
+      a: "Object-Oriented",
+      b: "Object-Based",
+      c: "Assembly-language",
+      d: "High-level",
+      ans: "b",
+    },
+    {
+      que: "Which one of the following also known as Conditional Expression: ",
+      a: "Alternative to if-else",
+      b: "Switch statement",
+      c: "If-then-else statement",
+      d: "immediate if",
+      ans: "d",
+    },
+    {
+      que: "In JavaScript, what is a block of statement?",
+      a: "Conditional block",
+      b: "block that combines a number of statements into a single compound statement",
+      c: "both conditional block and a single statement",
+      d: "block that contains a single statement",
+      ans: "b",
+    },
+    {
+      que: "When interpreter encounters an empty statements, what it will do:",
+      a: "Shows a warning",
+      b: "Prompts to complete the statement",
+      c: "Throws an error",
+      d: "Ignores the statements",
+      ans: "d",
+    },
+    {
+      que: "Which one of the following also known as Conditional Expression: ",
+      a: "Alternative to if-else",
+      b: "Switch statement",
+      c: "If-then-else statement",
+      d: "immediate if",
+      ans: "d",
+    },
+    {
+      que: "In JavaScript, what is a block of statement?",
+      a: "Conditional block",
+      b: "block that combines a number of statements into a single compound statement",
+      c: "both conditional block and a single statement",
+      d: "block that contains a single statement",
+      ans: "b",
+    },
+    {
+      que: "Which of the following type of a variable is volatile?",
+      a: "Mutable variable",
+      b: "Dynamic variable",
+      c: "Volatile variable",
+      d: "Immutable variable",
+      ans: "a",
+    },
+    {
+      que: "Which one of the following also known as Conditional Expression: ",
+      a: "Alternative to if-else",
+      b: "Switch statement",
+      c: "If-then-else statement",
+      d: "immediate if",
+      ans: "d",
+    },
+    {
+      que: "Which of the following number object function returns the value of the number?",
+      a: "toString()",
+      b: "valueOf()",
+      c: "toLocaleString()",
+      d: "toPrecision()",
+      ans: "b",
+    },
+    {
+      que: "In the JavaScript, which one of the following is not considered as an error:",
+      a: "Syntax error",
+      b: "Missing of semicolons",
+      c: "Division by zero",
+      d: "Missing of Bracket",
+      ans: "c",
+    },
+  ]),
+  (science = [
+    {
+      que: " Which of the following oxides of iron would be obtained on the prolonged reaction of iron with steam?",
+      a: "FeO",
+      b: "Fe2O3",
+      c: "Fe3O4",
+      d: " Fe2O3 and Fe3O4",
+      ans: "c",
+    },
+    {
+      que: "A non-metal used to preserve food material is:",
+      a: "Carbon",
+      b: "Phosphorus",
+      c: "Sulphurr",
+      d: "Nitrogen",
+      ans: "d",
+    },
+    {
+      que: "The arrangement for Copper, Tin, Lead and Mercury, according to the reactivity series, is:",
+      a: "Tin> Lead> Copper> Mercury",
+      b: "Lead> Copper> Mercury> Tin",
+      c: " Copper> Mercury> Tin> Lead",
+      d: " Mercury> Tin> Lead> Copper",
+      ans: "a",
+    },
+    {
+      que: " The metals that float when treated with water are:",
+      a: "Manganese and sodium",
+      b: "Sodium and calcium",
+      c: " Magnesium and sodium",
+      d: "Magnesium and calcium",
+      ans: "d",
+    },
+    {
+      que: "When calcium oxide is added to water, it completely dissolves in water without forming bubbles. What products are formed in this reaction?",
+      a: "Ca and H2",
+      b: "Ca and H2O2",
+      c: "Ca(OH)2",
+      d: "CaH2",
+      ans: "c",
+    },
+    {
+      que: "Pick the statement which is correct about the non-metal. ",
+      a: "Br is an example of a liquid non-metal.",
+      b: "Graphite is a good conductor of electricity",
+      c: "Most of the non-metal oxides are acidic",
+      d: " All of these",
+      ans: "d",
+    },
+    {
+      que: "When a non-metal is allowed to react with water:",
+      a: "CO2 gas is formed",
+      b: "H2 gas is formed",
+      c: "Product formed depends on the temperature",
+      d: "No products are formed",
+      ans: "d",
+    },
+    {
+      que: "Aqua regia is a freshly prepared mixture of concentrated HNO3 and concentrated HCl in the ratio of:",
+      a: " 1:3, respectively",
+      b: " 2:3, respectively",
+      c: "3:1, respectively",
+      d: "3:2, respectively",
+      ans: "a",
+    },
+    {
+      que: "What is the capital of Tamil Nadu?",
+      a: "delhi",
+      b: "mumbai",
+      c: "chennai",
+      d: "kolkata",
+      ans: "c",
+    },
+    {
+      que: "What is the capital of West Bengal?",
+      a: "delhi",
+      b: "mumbai",
+      c: "chennai",
+      d: "kolkata",
+      ans: "d",
+    },
+  ]),
+  (GenralKwoledge = [
+    {
+      que: "What is the capital of India?",
+      a: "delhi",
+      b: "mumbai",
+      c: "chennai",
+      d: "kolkata",
+      ans: "a",
+    },
+    {
+      que: "Which Planet is known As the Red Planet ",
+      a: "Venus",
+      b: "Mars",
+      c: "Jupiter",
+      d: "Saturn",
+      ans: "b",
+    },
+    {
+      que: "What is the capital of Tamil Nadu?",
+      a: "delhi",
+      b: "mumbai",
+      c: "chennai",
+      d: "kolkata",
+      ans: "c",
+    },
+    {
+      que: "What is the capital of West Bengal?",
+      a: "delhi",
+      b: "mumbai",
+      c: "chennai",
+      d: "kolkata",
+      ans: "d",
+    },
+  ]),
+];
+
+let userName = document.querySelector("#userName").value;
+const submit = document.querySelector("#submitBtn");
+
+let index = 0;
+let subset = 0;
+let total = questionSet.length;
+let right = 0;
+let timmer = 10;
+let wrong = 0;
+let questionCount = 1;
+let questionCategory = [];
+let PlayerName = [];
+let score=0;
+
+//our project start from here submit button.check the category selection and input field are filled well. after submit button click. the selction category call..
+submit.addEventListener("click", () => {
+  userName = document.querySelector("#userName").value;
+  if (userName == "") {
+    alert("please fill the blank");
+  } else {
+    selectionCategory();
+  }
+});
+
+//before starting quize we confirm that our question Category and input field are fill..
+function selectionCategory() {
+  alert("please select the category");
+  let Category = document.querySelectorAll(".categoryOption");
+  Category.forEach((cate) => {
+    cate.addEventListener("click", function (data) {
+      let quesCategory = data.target.id;
+      questionCategory.push(quesCategory);
+      console.log(questionCategory);
+      start();
+    });
+  });
+}
+
+//here we collect playerName in array via push mathod and call for question load.and with question load we will start countdwon
+function start() {
+  let userName = document.querySelector("#userName").value;
+  let arry = PlayerName.push(userName);
+  console.log(PlayerName);
+  PlayerName = "";
+  getQuestion();
+}
+
+//here i face one problem due to scop issue..
+//i take setInterval in a variable.but when i call this coutdwon on nextquestion that time i can't clear this time out..due to this parallel countDwon function running seperatly.
+function countDwonStater() {
+  const startTimer = setInterval(function () {
+  counter=document.querySelector("#countDwon");
+  counter.innerText=timmer;
+    if (timmer == 0) {
+      clearInterval(startTimer);
+      timmer = 10;
+    }
+    timmer--;
+    return;
+  }, 1000);
+}
+
+// question is depend on question category.i predefine the index of arrya before it load
+function getQuestion() {
+  if (questionCategory == "history") {
+    index += 0;
+  } else if (questionCategory == "JavaScript") {
+    index += 1;
+  } else if (questionCategory == "science") {
+    index += 2;
+  } else {
+    questionCategory == "GenralKwoledge";
+  }
+  loadQuestion();
+}
+
+
+
+
+//here i done another mistake i'll take mainContent Hardcore.it is working but whole HTMl code written here.
+function loadQuestion() {
+  countDwonStater();
+  QuestionData = questionSet[index][subset];
+  displayQuestion = document.querySelector(".questionBox");
+  document.querySelector(".mainContainer").innerHTML = `
+  <div class="quizeBody">
+    <div class="quizeContainer">
+      
+      <div class="header">
+      <h3 id="questionCategory">Question Category is "${questionCategory}"</h3>
+      <div class="scoreTime">
+        <div class="time">
+          <label for="" id="countDwon"></label>
+        </div>
+        <div class="score">
+          <label for="">Score: ${score} </label>
+        </div>
+      </div>
+     
+
+      <div class="questionContainer">
+        <div class="questionCount">
+          <h3>${questionCount}/${questionSet[index].length}</h3>
+        </div>
+        <div class="questionText">
+          <h2 id='questionBox'>${QuestionData.que}</h2>
+        </div>
+      </div>
+
+      <div class="answerContainer">
+        <button id="a" class="options">${QuestionData.a}</button>
+        <button id="b" class="options">${QuestionData.b}</button>
+        <button id="c" class="options">${QuestionData.c}</button>
+        <button id="d" class="options">${QuestionData.d}</button>
+      </div>
+
+      <button id="nextQuestion" onClick="nextQue()">Next Question</button>
+    </div>
+   </div>
+    
+  </div>
+  `;
+  
+
+  // this code is use for selection of Answer.and updating score.
+ let answerData = document.querySelectorAll(".options");
+  answerData.forEach((dataBtn) => {
+    dataBtn.addEventListener("click", (dataBtn) => {
+      console.log(dataBtn.target.id);
+      if(QuestionData.ans==dataBtn.target.id){
+            console.log("congratulation");
+            score++;
+            right++;
+      dataBtn.target.style.backgroundColor='green';
+      }else{
+        wrong++;
+          dataBtn.target.style.backgroundColor='red';
+          answerData.disabled='true'
+      }
+      
+    });
+  });
+return;
+}
+
+
+
+function ResultPage() {
+  let scorePercentage=eval(`${questionSet[index].length}*${score}%${100}`);
+  document.querySelector(".mainContainer").innerHTML = `
+        <div class="resultContainer3rd">
+          <h2 class="resultHeading3rdPage">Quiz Result</h2>
+          <h4 class="resultHeading4-3rdPage">${userName}:<h5>your result is</h5></h4>
+          <h4 class="resultHeading4-3rdPage">Total time taken:110 second</h4>
+          <h4 class="resultHeading4-3rdPage">total Question:${questionSet[index].length}</h4>
+           <h4 class="resultHeading4-3rdPage">Attempt:10</h4>
+          <h4 class="resultHeading4-3rdPage">Correct:${right}</h4>
+           <h4 class="resultHeading4-3rdPage">Wrong:${wrong}</h4>
+           <h4 class="resultHeading4-3rdPage">percentage:${scorePercentage}%</h4>
+       <div class="resultPagebutton">
+           <button class="resultButton-3rdPage"><a href="http://127.0.0.1:5500/index.html">Start Again</a></button>
+            <button class="resultButton-3rdPage"><a href="http://127.0.0.1:5500/index.html">Home</a></button>
+            
+       </div>
+      </div>`;}
+
+function nextQue() {
+  subset++;
+  questionCount++;
+  //i want clear this countdwon before the function execute but due to scop issue i am able to solve this ..i will solve this bug latter.
+  if (subset < questionSet[index].length) {
+    loadQuestion();
+  } else {
+    ResultPage();
+  }
+}
